a2466265cfe05ad3e5e25f231e305e8c1a7070d2
hi-rev <hirev05@gmail.com>
2023-02-15 15:54:53 +0900

    springboot-myweb #1

--- springboot-myapplication/src/test/java/ex04/MyApplicationTest.java
+++ springboot-myapplication/src/test/java/ex04/MyApplicationTest.java
@@ -1,22 +1,14 @@
-package ex03;
+package ex04;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import ex03.component.MyComponent;
+import ex04.component.MyComponent;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * 
- * Spring Boot Test Integration
- * 
- * Spring Boot Test Integration(@SpringBootTest) 에서는
- * 자동 스캔을 위해서는 설정 클래스에 @SpringBootConfiguration을 달고 있어야 한다. 
- *
- */
 @SpringBootTest
 public class MyApplicationTest {
 	
